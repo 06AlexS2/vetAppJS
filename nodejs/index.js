c3060d86079362cf4dab92badf887bd9ef3739fb
06AlexS2 <sanchez.alejandro0603@gmail.com>
2023-02-09 17:09:30 -0600

    intro to node.js module finished

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,8 @@
 const http = require('node:http'); //si no se guarda en una constante
 //no tengo como referenciarme a el para su uso posterior
 const url = require('url')
+//crear stringDecoder para recibir objetos
+const stringDecoder = require('string_decoder').StringDecoder;
 
 //el objeto o prototipo http tiene un método que se llama por el punto .
 const serverCallback = (req, res) => {
@@ -32,23 +34,78 @@ const serverCallback = (req, res) => {
 
     //paso 3.3: obtener los headers desde el request
     const { headers } = req;
-    console.log({ headers });
+   
+    //paso 3.4: obtener payload en el caso de haberlo
+    //va a obtener una fuente de información (stream) y la idea es irlos recibiendo e irlos poniendo
+    //en su lugar, de forma que yo entiendo el mensaje
+    const decoder = new stringDecoder('utf-8');
+    let buffer = '';
+    //3.4.1 ir acumulando la data cuando el request reciba un payload, o
+    //request empieza a escuchar cuando le lleguen datos de un payload
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    //3.4.2 terminar de acumular datos y decirle al decoder que finalice
+    //o le indico al decoder que ya no reciba más datos
+    req.on('end', () => {
+        buffer += decoder.end();
 
-    //paso 4: enviar una respuesta dependiendo de la ruta
-    /*NOTA: te daba el error 'ERR_STREAM_WRITE_AFTER_END' porque despues de un res.end()
-     debes poner return;, de lo contrario, finalizará el stream de requests en el servidor 
-     exhibit A: */
-    switch(cleanRoute){
-        case 'ruta':
-            res.end("estas en una ruta conocida")
-            return;
-        default:
-            res.end('esta ruta no es conocida')
-            return;
-    }
+        //paso 3.5 ordenar los datos de respuesta
+        //estoy creando lo que llega en request pero de una forma legible para el desarrollador
+        const data = {
+            route: cleanRoute,
+            query,
+            method,
+            headers,
+            payload: buffer
+        };
 
-    res.end("hola mundo en un server http")
+        //paso 3.6 elegir el manejador de la respuesta dependiendo de la ruta y asignarle la función que el enrutador tiene (handler)
+        let handler;
+        //si ruta limpia y enrutador en la posicion de ruta limpia existe:
+        if(cleanRoute && router[cleanRoute]){
+            //dentro de handler estará la funcion que se mande
+            handler = router[cleanRoute];
+        } else {
+            handler = router.notFounded;
+        }
+
+        //paso 4: enviar una respuesta dependiendo de la ruta
+        /*NOTA: te daba el error 'ERR_STREAM_WRITE_AFTER_END' porque despues de un res.end()
+        debes poner return;, de lo contrario, finalizará el stream de requests en el servidor 
+        exhibit A: */
+        /*switch(cleanRoute){
+            case 'ruta':
+                res.end("estas en una ruta conocida")
+                return;
+            default:
+                res.end('esta ruta no es conocida')
+                return;
+        }*/
+
+        if(typeof handler === 'function') {
+            handler(data, (statusCode = 200, message) => {
+                const response = JSON.stringify(message);
+                res.writeHead(statusCode);
+                //linea donde realmente ya estamos respondiendo a la aplicacion cliente
+                res.end(response);
+            });
+        }
+    });
 };
+
+const router = {
+    ruta: (data, callback) => { //esto es un handler, se ve mas a detalle en express.js
+        callback(200, {message: 'está es /ruta'})
+    },
+    users: (data, callback) => { 
+        callback(200, [{name: 'user 1'}, {name: 'user 2'}])
+    },
+
+    notFounded: (data, callback) => {
+        callback(404, {message: 'no encontrado'});
+    }
+}
 
 //la constante server es lo que va a utilizarse para ejecutar
 const server = http.createServer(serverCallback);
